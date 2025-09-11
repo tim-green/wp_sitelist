1cecd6260966daa5a31252d53a9c2f623490dd44
Tim <github@timgreen.ws>
2025-09-11 12:11:03 +1000

    added vite

---
+++ vite.config.mjs
@@ -0,0 +1,25 @@
+//vite.config.mjs
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+// Get the main.js where all your JavaScript files are imported
+const JS_FILE = resolve('assets/js/app.js')
+
+// Define where the compiled and minified JavaScript files will be saved
+const BUILD_DIR = resolve(__dirname, 'assets/build/');
+
+export default defineConfig({
+  build: {
+    assetsDir: '', // Will save the compiled JavaScript files in the root of the dist folder
+    emptyOutDir: true, // Empty the dist folder before building
+    outDir: BUILD_DIR,
+    rollupOptions: {
+      input: JS_FILE,
+      output: {
+        entryFileNames: `[name].min.js`,
+        chunkFileNames: `[name].min.js`,
+        assetFileNames: `[name].min.css`
+      }
+    },
+  },
+});
