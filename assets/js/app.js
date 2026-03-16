71889c809b63d51466ccefd1987063b65befa576
Tim <github@timgreen.ws>
2026-03-16 11:22:10 +1100

    added app.js

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// assets/js/app.js
+
+/**
+ * Main JS file for Sitelist
+ * 
+ * Import the main css file
+ * Import all the JavaScript files that are needed for the theme.
+ */
+
+// Import the main CSS file
+import '../sass/app.scss'
+
+// Build the main JS file
+// ... other js files imports
